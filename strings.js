7bb8bc6e53eae4cba709a13a8f2ea0d847bdd8cd
tejman <rudrarajut@gmail.com>
2014-01-16 10:30:28 -0700

    first commit

---
+++ strings.js
@@ -0,0 +1,12 @@
+var word = prompt("Enter a word: ");
+
+alert(
+	"You entered: " + word + "\n" +
+	"There are " + word.length + " characters in the word." + "\n" +
+	"The third character is " + "'" + word.charAt(2) + "'" + "\n" +
+	"Lowercase: " + word.toLowerCase() + "\n" +
+	"Uppercase: " + word.toUpperCase() + "\n" +
+	"Example: Here is " + word + " in a sentence." + "\n" +
+	"Subword: " + word.substring(1,4)
+	);
+
